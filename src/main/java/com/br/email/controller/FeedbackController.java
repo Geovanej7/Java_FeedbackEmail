880717bc31ff9e6aa119585b93c3c64f7152c5e4
Geo <gjsj@discente.ifpe.edu.br>
2025-04-14 11:03:16 -0300

    finish

--- src/main/java/com/br/email/controller/FeedbackController.java
+++ src/main/java/com/br/email/controller/FeedbackController.java
@@ -1,26 +1,24 @@
 package com.br.email.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.br.email.model.dto.FeedbackDto;
 import com.br.email.service.EmailService;
-
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/feedback")
-@CrossOrigin(origins = "*") 
 public class FeedbackController {
      @Autowired
     private EmailService emailService;
 
     @PostMapping
-    public String enviarFeedback(@RequestBody @Valid FeedbackDto dto) {
-        return emailService.enviarEmailFeedback(dto);
+    public ResponseEntity<?> enviarFeedback(@RequestBody @Valid FeedbackDto dto) {
+        emailService.enviarEmailFeedback(dto);
+        return ResponseEntity.ok().build();
     }
 }
